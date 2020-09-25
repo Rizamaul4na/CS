6651aa453b8bc901e0acfde4948c414c3f8e3e06
Rizamaul4na <53503218+Rizamaul4na@users.noreply.github.com>
2020-09-25 16:10:41 +0700

    Add files via upload

---
+++ biodata.cs
@@ -0,0 +1,27 @@
+using System;
+					
+public class Program
+{
+	public static void Main(string []args)
+	{
+		Console.WriteLine("===BIODATA===");
+		Console.Write("Nama : ");
+		Console.Write("Riza Maulana");
+			Console.WriteLine("");
+		Console.Write("Kelas : ");
+		Console.Write("XII RPL 1");
+			Console.WriteLine("");
+		Console.Write("Sekolah : ");
+		Console.Write("SMKN 1 Subang");
+			Console.WriteLine("");
+		Console.Write("Alamat : ");
+		Console.Write("Soklat, Subang");
+			Console.WriteLine("");
+		Console.Write("TTL : ");
+		Console.Write("Subang, 29 Mei 2003");
+			Console.WriteLine("");
+		Console.Write("Hobi : ");
+		Console.Write("Mendengarkan Musik");
+		
+	}
+}
